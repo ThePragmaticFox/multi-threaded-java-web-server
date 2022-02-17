83cddebf3652fb30b41e540368576586bc69be8e
PragmaticFox <rwitschi@moocloud.ch>
2022-02-17 19:25:31 +0100

    unnecessary import

--- src/main/java/com/server/HTTPResponseHandler.java
+++ src/main/java/com/server/HTTPResponseHandler.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.nio.file.Files;
 import org.apache.commons.io.FilenameUtils;
 import java.io.IOException;
-import java.io.OutputStream;
 
 public class HTTPResponseHandler {
 
