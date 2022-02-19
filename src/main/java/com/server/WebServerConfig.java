6e1e12e6c66e61667e87453f7dac4062995f3f4b
PragmaticFox <rwitschi@moocloud.ch>
2022-02-19 17:04:18 +0100

    Use more sensible values for the threadpool

--- src/main/java/com/server/WebServerConfig.java
+++ src/main/java/com/server/WebServerConfig.java
@@ -14,8 +14,8 @@ public class WebServerConfig {
     private static final String ROOT = "www";
     private static final String HOST = "0.0.0.0";
     private static final int PORT = 3000;
-    private static final int NB_POOL_THREADS = 250;
-    private static final int BACKLOG_SIZE = 10000;
+    private static final int NB_POOL_THREADS = 3;
+    private static final int BACKLOG_SIZE = 1000;
 
     private final String root;
     private final String host;
