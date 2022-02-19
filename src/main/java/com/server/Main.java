364a1038cc09dd271670c7fce3b656d3fb1fc88a
Pragmatic Fox <85360470+ThePragmaticFox@users.noreply.github.com>
2022-02-19 23:06:30 +0100

    Update Main.java

--- src/main/java/com/server/Main.java
+++ src/main/java/com/server/Main.java
@@ -5,7 +5,6 @@ import java.util.Optional;
 
 public class Main {
     public static void main(String[] args) {
-        //Arrays.stream(Logger.getLogger("Global").getHandlers()).forEach(handler -> handler.setLevel(Level.FINEST));
         final String path = args.length == 2 && args[0].equals("-f") ? args[1] : "config.json";
         final WebServerConfig webServerConfig = WebServerConfig.importConfig(path);
         final Optional<WebServer> webServer = WebServer.start(webServerConfig);
