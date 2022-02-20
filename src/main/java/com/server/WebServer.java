e15e0e89a58f61dffc1df0d6451a037e94856cf1
PragmaticFox <rwitschi@moocloud.ch>
2022-02-20 13:21:30 +0100

    DONE

--- src/main/java/com/server/WebServer.java
+++ src/main/java/com/server/WebServer.java
@@ -20,7 +20,7 @@ public class WebServer implements Runnable {
     public WebServer(final WebServerConfig webServerConfig) throws IOException {
         config = webServerConfig;
         isRunning = new AtomicBoolean(true);
-        serverSocket = new ServerSocket(config.getPort(), 25000,
+        serverSocket = new ServerSocket(config.getPort(), config.getBacklogSize(),
                 InetAddress.getByName(config.getHost()));
         serverThreadPool = Executors.newFixedThreadPool(config.getNbPoolThreads());
     }
